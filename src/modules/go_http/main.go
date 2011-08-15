dcd34cf40508a3a958626a48c53c0c2127be6c2e
x86 <x86mail@gmail.com>
2011-08-15 21:23:05 +0400

    Module go_http fixed

--- src/modules/go_http/main.go
+++ src/modules/go_http/main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"io"
 	"os"
-	//"net"
+	"net"
 	"log"
 	"http"
 	f "gofrozen"
@@ -16,30 +15,21 @@ type myHttp struct {
 }
 	func (srv *myHttp) Run(addr string) os.Error {
 		mux := http.NewServeMux()
-		//mux.HandleFunc("/", func (w http.ResponseWriter, req *http.Request){
-		//	h := f.Hash([]f.Hskel {
-		//		f.Hitem(f.HK_url, f.TYPE_STRINGT, req.URL.RawPath ),
-		//		f.Hend() });
-		//
-		//	f.Backend_query(srv.backend, h)
-		//})
-		mux.HandleFunc("/", handleFunc)
+		mux.HandleFunc("/", func (w http.ResponseWriter, req *http.Request){
+			h := f.Hash([]f.Hskel {
+				f.Hitem(f.HK_action, f.TYPE_UINT32T, f.ACTION_CRWD_CREATE ),
+				f.Hitem(f.HK_url,    f.TYPE_STRINGT, req.URL.RawPath ),
+				f.Hend() });
 
-		//l, e := net.Listen("tcp", addr)
-		//if e != nil {
-		//	return e
-		//}
-		//go http.Serve(l, mux)
-		err := http.ListenAndServe(addr, mux)
-		if err != nil {
-			log.Print(err)
+			f.Backend_query(srv.backend, h)
+		})
+
+		l, e := net.Listen("tcp", addr)
+		if e != nil {
+			return e
 		}
+		go http.Serve(l, mux)
 		return nil
-	}
-
-	func handleFunc(w http.ResponseWriter, req *http.Request){
-		io.WriteString(w, "Hello")
-		log.Print("Hello")
 	}
 
 func http_init(backend uintptr) int{
@@ -47,14 +37,12 @@ func http_init(backend uintptr) int{
 }
 func http_configure(backend uintptr, config uintptr) int{
 	addr, ok := f.Hget(config, f.HK_addr).(string)
-	log.Printf("addr %v", []byte(addr))
 
 	srv := &myHttp{ backend: backend }
 	if e := srv.Run(addr); e != nil {
-		log.Print("Failed %v", e)
+		log.Print("go_http failed configure: %v", e)
 		return -1
 	}
-	log.Print("Ok")
 	return 0
 }
 func http_destroy(backend uintptr) int{
@@ -68,34 +56,6 @@ func main(){
 	g.SetFunc_configure(http_configure)
 	g.SetFunc_destroy(http_destroy)
 	f.Class_register(g.Swigcptr())
-	
-	//srv := &myHttp{ }
-	//if e := srv.Run(":12345"); e != nil {
-	//	log.Print("Failed %v", e)
-	//	return
-	//}
-	//log.Print("Ok")
-	
 	return
 }
 
-/*
-	g.GetBackend_type_crwd().SetFunc_count(http_count)
-
-func http_count(backend uintptr, hash uintptr) int{
-	log.Print("go http count")
-	n := f.Hash([]f.Hskel {
-		f.Hitem(f.HK_backend, f.TYPE_STRINGT, "mooz" ),
-		f.Hnext(hash) });
-
-	return f.Backend_pass(backend, n)
-}
-	config  := f.Configs_string_parse(` { class="go_http" } `)
-	backend := f.Backend_new(config)
-
-	h := f.Hash([]f.Hskel {
-		f.Hitem(f.HK_action, f.TYPE_UINT32T, f.ACTION_CRWD_COUNT ),
-		f.Hend() });
-
-	f.Backend_query(backend, h)
-	*/
