9387e6ccc00e155274665301e75a28639c775fe1
x86 <x86mail@gmail.com>
2011-11-22 14:14:00 +0400

    Fixed bug in slider

--- src/libfrozen/data/slider/slider_t.h
+++ src/libfrozen/data/slider/slider_t.h
@@ -19,7 +19,7 @@
  *  @endcode
  */
 
-#define DATA_SLIDERT(_data,_off)  {TYPE_SLIDERT, (slider_t []){ { _data, _off } }}
+#define DATA_SLIDERT(_data,_off)  {TYPE_SLIDERT, (slider_t []){ { _data, _off, ~0 } }}
 #define DEREF_TYPE_SLIDERT(_data) (slider_t *)((_data)->ptr)
 #define REF_TYPE_SLIDERT(_dt) _dt
 #define HAVEBUFF_TYPE_SLIDERT 0
