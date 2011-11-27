fda299078c31706ed5f2a046ab734e59fd06bf66
x86 <x86mail@gmail.com>
2011-11-27 13:12:37 +0400

    'data/transfer' now passive, use 'daemon/thread' to start transfer

--- src/libfrozen/backends/data/transfer/transfer.c
+++ src/libfrozen/backends/data/transfer/transfer.c
@@ -8,7 +8,7 @@
  * @ingroup mod_backend_transfer
  * @page page_transfer_info Description
  *
- * This module actively transfer data from one backend to another
+ * This is passive module which transfer data from one backend to another on every incoming request.
  */
 /**
  * @ingroup mod_backend_transfer
@@ -29,11 +29,35 @@
 typedef struct transfer_userdata {
 	backend_t             *source;
 	backend_t             *destination;
-	uintmax_t              running;
-	pthread_t              tranfser_thread;
 } transfer_userdata;
 
-static void * tranfser_thread_handler(backend_t *backend){ // {{{
+static int transfer_init(backend_t *backend){ // {{{
+	if((backend->userdata = calloc(1, sizeof(transfer_userdata))) == NULL)
+		return error("calloc failed");
+	
+	return 0;
+} // }}}
+static int transfer_destroy(backend_t *backend){ // {{{
+	transfer_userdata        *userdata          = (transfer_userdata *)backend->userdata;
+	
+	if(userdata->source)
+		backend_destroy(userdata->source);
+	if(userdata->destination)
+		backend_destroy(userdata->destination);
+	
+	free(userdata);
+	return 0;
+} // }}}
+static int transfer_configure(backend_t *backend, hash_t *config){ // {{{
+	ssize_t                ret;
+	transfer_userdata     *userdata          = (transfer_userdata *)backend->userdata;
+	
+	hash_data_copy(ret, TYPE_BACKENDT,   userdata->source,        config, HK(source));
+	hash_data_copy(ret, TYPE_BACKENDT,   userdata->destination,   config, HK(destination));
+	return 0;
+} // }}}
+
+static ssize_t transfer_handler(backend_t *backend, request_t *request){ // {{{
 	backend_t             *source;
 	backend_t             *destination;
 	transfer_userdata     *userdata          = (transfer_userdata *)backend->userdata;
@@ -45,64 +69,23 @@ static void * tranfser_thread_handler(backend_t *backend){ // {{{
 	data_t  d_destination = DATA_BACKENDT(destination);
 	
 	fastcall_transfer r_transfer = { { 3, ACTION_TRANSFER }, &d_destination };
-	data_query(&d_source, &r_transfer);
-	
-	// TODO on_end motion
-	return NULL;
+	return data_query(&d_source, &r_transfer);
 } // }}}
-
-static int transfer_init(backend_t *backend){ // {{{
-	if((backend->userdata = calloc(1, sizeof(transfer_userdata))) == NULL)
-		return error("calloc failed");
-	
-	return 0;
-} // }}}
-static int transfer_destroy(backend_t *backend){ // {{{
-	ssize_t                   ret;
-	void                     *res;
-	transfer_userdata        *userdata          = (transfer_userdata *)backend->userdata;
-	
-	if(userdata->running != 0){
-		switch(pthread_cancel(userdata->tranfser_thread)){
-			case ESRCH: goto no_thread;
-			case 0:     break;
-			default:    ret = error("pthread_cancel failed"); goto no_thread;
-		}
-		if(pthread_join(userdata->tranfser_thread, &res) != 0)
-			ret = error("pthread_join failed");
-		
-		no_thread:
-		userdata->running = 0;
-	}
-	if(userdata->source)
-		backend_destroy(userdata->source);
-	if(userdata->destination)
-		backend_destroy(userdata->destination);
-	
-	free(userdata);
-	return ret;
-} // }}}
-static int transfer_configure(backend_t *backend, hash_t *config){ // {{{
-	ssize_t                ret;
-	transfer_userdata     *userdata          = (transfer_userdata *)backend->userdata;
-	
-	hash_data_copy(ret, TYPE_BACKENDT,   userdata->source,        config, HK(source));
-	hash_data_copy(ret, TYPE_BACKENDT,   userdata->destination,   config, HK(destination));
-	
-	if(userdata->running == 0){
-		if(pthread_create(&userdata->tranfser_thread, NULL, (void *(*)(void *)) &tranfser_thread_handler, backend) != 0)
-			ret = error("pthread_create failed");
-		
-		userdata->running = 1;
-	}
-	return 0;
+static ssize_t transfer_fast_handler(backend_t *backend, void *hargs){ // {{{
+	return transfer_handler(backend, NULL);
 } // }}}
 
 backend_t transfer_proto = {
 	.class          = "data/transfer",
-	.supported_api  = API_HASH,
+	.supported_api  = API_HASH | API_FAST,
 	.func_init      = &transfer_init,
 	.func_configure = &transfer_configure,
 	.func_destroy   = &transfer_destroy,
+	.backend_type_hash = {
+		.func_handler = &transfer_handler
+	},
+	.backend_type_fast = {
+		.func_handler = &transfer_fast_handler
+	}
 };
 
