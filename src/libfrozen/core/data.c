652be3eac51d0072173f75753613d7c113723f51
x86 <x86mail@gmail.com>
2012-01-04 21:40:17 +0400

    Data default handler for API_HANDLERS

--- src/libfrozen/core/data.c
+++ src/libfrozen/core/data.c
@@ -21,8 +21,13 @@ static void    data_fill_blanks(data_proto_t *p){ // {{{
 			break;
 		case API_HANDLERS:
 			for(j=0; j<sizeof(p->handlers)/sizeof(p->handlers[0]); j++){
-				if(!p->handlers[j])
-					p->handlers[j] = data_protos[ TYPE_DEFAULTT ]->handlers[j];
+				if(!p->handlers[j]){
+					if(p->handler_default){
+						p->handlers[j] = p->handler_default;
+					}else{
+						p->handlers[j] = data_protos[ TYPE_DEFAULTT ]->handlers[j];
+					}
+				}
 			}
 			break;
 	}
