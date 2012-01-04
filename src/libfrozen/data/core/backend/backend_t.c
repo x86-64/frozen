0690d02979b2c27ca78ef53e8f43de0e8c59ea89
x86 <x86mail@gmail.com>
2012-01-04 21:42:52 +0400

    backend_t default handler will pass fast requests to backend

--- src/libfrozen/data/core/backend/backend_t.c
+++ src/libfrozen/data/core/backend/backend_t.c
@@ -5,8 +5,8 @@
 #include <core/hash/hash_t.h>
 #include <enum/format/format_t.h>
 
-static ssize_t data_backend_t_io  (data_t *data, fastcall_io *fargs){ // {{{
-	return backend_fast_query((backend_t *)data->ptr, fargs);
+static ssize_t data_backend_t_default(data_t *data, void *hargs){ // {{{
+	return backend_fast_query((backend_t *)data->ptr, hargs);
 } // }}}
 static ssize_t data_backend_t_convert_from(data_t *dst, fastcall_convert_from *fargs){ // {{{
 	ssize_t                ret;
@@ -71,11 +71,10 @@ data_proto_t backend_t_proto = {
 	.type                   = TYPE_BACKENDT,
 	.type_str               = "backend_t",
 	.api_type               = API_HANDLERS,
+	.handler_default        = (f_data_func)&data_backend_t_default,
 	.handlers = {
 		[ACTION_PHYSICALLEN]  = (f_data_func)&data_backend_t_len,
 		[ACTION_LOGICALLEN]   = (f_data_func)&data_backend_t_len,
-		[ACTION_READ]         = (f_data_func)&data_backend_t_io,
-		[ACTION_WRITE]        = (f_data_func)&data_backend_t_io,
 		[ACTION_CONVERT_FROM] = (f_data_func)&data_backend_t_convert_from,
 		[ACTION_FREE]         = (f_data_func)&data_backend_t_free,
 		[ACTION_ALLOC]        = (f_data_func)&data_backend_t_alloc,
