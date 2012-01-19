c7caca2e53eaf8b34be94605a621ff005fcadfd4
x86 <x86mail@gmail.com>
2012-01-19 19:20:30 +0400

    Zeromq fix

--- src/modules/c_zmq/main.c
+++ src/modules/c_zmq/main.c
@@ -1,55 +1,19 @@
 #include <libfrozen.h>
 #include <zmq.h>
-
-/**
- * @ingroup machine
- * @addtogroup mod_machine_zmq module/c_zmq
- */
-/**
- * @ingroup mod_machine_zmq
- * @page page_zmq_info Description
- *
- * This module implement stubs to work with ZeroMQ.
- */
-/**
- * @ingroup mod_machine_zmq
- * @page page_zmq_config Configuration
- * 
- * Accepted configuration:
- * @code
- * {
- *              class                   = "modules/c_zmq",
- *              type                    =                     # socket type to create, see man zmq_socket
- *                                        "req",
- *                                        "rep",
- *                                        "dealer",
- *                                        "router",
- *                                        "pub",
- *                                        "sub",
- *                                        "push",
- *                                        "pull",
- *                                        "pair",
- *              identity                = "ident",            # set socket identity
- *              bind                    = "tcp://",           # bind socket to            OR
- *              connect                 = "tcp://".           # connect socket to
- *
- *
- * }
- * @endcode
- */
+#include <zeromq_t.h>
 
 #define EMODULE 101
 #define ZMQ_NTHREADS        1
 
-typedef struct zmq_userdata {
+typedef struct zeromq_t {
 	void                  *zmq_socket;
-} zmq_userdata;
+} zeromq_t;
 
 void                          *zmq_ctx           = NULL;
-uintmax_t                      zmq_machines      = 0;
-pthread_mutex_t                zmq_machines_mtx  = PTHREAD_MUTEX_INITIALIZER;
+uintmax_t                      zmq_data          = 0;
+pthread_mutex_t                zmq_data_mtx      = PTHREAD_MUTEX_INITIALIZER;
 
-static int zmq_string_to_type(char *type_str){ // {{{
+static ssize_t zeromq_t_string_to_type(char *type_str){ // {{{
 	if(type_str != NULL){
 		if(strcasecmp(type_str, "req") == 0) return ZMQ_REQ;
 		if(strcasecmp(type_str, "rep") == 0) return ZMQ_REP;
@@ -63,235 +27,206 @@ static int zmq_string_to_type(char *type_str){ // {{{
 	}
 	return -1;
 } // }}}
-
-static int zmqb_init(machine_t *machine){ // {{{
-	ssize_t                ret               = 0;
-	zmq_userdata          *userdata;
+static ssize_t zeromq_t_destroy(zeromq_t *fdata){ // {{{
+	if(fdata->zmq_socket)
+		zmq_close(fdata->zmq_socket);
 	
 	// manage global zmq context
-	pthread_mutex_lock(&zmq_machines_mtx);
-		if(!zmq_ctx){
-			if( (zmq_ctx = zmq_init(ZMQ_NTHREADS)) == NULL){
-				ret = error("zmq_init failed");
-				goto error;
-			}
-		}
-		zmq_machines++;
-	pthread_mutex_unlock(&zmq_machines_mtx);
-	
-	if((userdata = machine->userdata = calloc(1, sizeof(zmq_userdata))) == NULL)
-		return error("calloc failed");
-	
-	return ret;
-
-error:
-	pthread_mutex_unlock(&zmq_machines_mtx);
-	return ret;
-} // }}}
-static int zmqb_destroy(machine_t *machine){ // {{{
-	zmq_userdata         *userdata          = (zmq_userdata *)machine->userdata;
-	
-	if(userdata->zmq_socket){
-		zmq_close(userdata->zmq_socket);
-	}
-	
-	// manage global zmq context
-	pthread_mutex_lock(&zmq_machines_mtx);
-		if(zmq_machines-- == 0){
+	pthread_mutex_lock(&zmq_data_mtx);
+		if(zmq_data-- == 0){
 			zmq_term(zmq_ctx);
 			zmq_ctx = NULL;
 		}
-	pthread_mutex_unlock(&zmq_machines_mtx);
+	pthread_mutex_unlock(&zmq_data_mtx);
 	
-	free(userdata);
+	free(fdata);
 	return 0;
 } // }}}
-static int zmqb_configure(machine_t *machine, config_t *config){ // {{{
+static ssize_t zeromq_t_new(zeromq_t **pfdata, config_t *config){ // {{{
 	ssize_t                ret;
 	int                    zmq_socket_type;
 	char                  *zmq_type_str      = NULL;
 	char                  *zmq_opt_ident     = NULL;
 	char                  *zmq_act_bind      = NULL;
 	char                  *zmq_act_connect   = NULL;
-	zmq_userdata          *userdata          = (zmq_userdata *)machine->userdata;
+	zeromq_t              *fdata;
+	
+	// manage global zmq context
+	pthread_mutex_lock(&zmq_data_mtx);
+		if(!zmq_ctx){
+			if( (zmq_ctx = zmq_init(ZMQ_NTHREADS)) == NULL){
+				pthread_mutex_unlock(&zmq_data_mtx);
+				return error("zmq_init failed");
+			}
+		}
+		zmq_data++;
+	pthread_mutex_unlock(&zmq_data_mtx);
+	
+	if((fdata = calloc(1, sizeof(zeromq_t))) == NULL)
+		return -ENOMEM;
 	
 	hash_data_copy(ret, TYPE_STRINGT, zmq_type_str,    config, HK(type));
 	hash_data_copy(ret, TYPE_STRINGT, zmq_act_bind,    config, HK(bind));
 	hash_data_copy(ret, TYPE_STRINGT, zmq_act_connect, config, HK(connect));
 	
-	if( (zmq_socket_type = zmq_string_to_type(zmq_type_str)) == -1 )
-		return error("invalid zmq type");
+	if( (zmq_socket_type = zeromq_t_string_to_type(zmq_type_str)) == -1 ){
+		ret = error("invalid zmq type");
+		goto error;
+	}
 	
-	if( (userdata->zmq_socket = zmq_socket(zmq_ctx, zmq_socket_type)) == NULL)
-		return error("zmq_socket failed");
+	if( (fdata->zmq_socket = zmq_socket(zmq_ctx, zmq_socket_type)) == NULL){
+		ret = error("zmq_socket failed");
+		goto error;
+	}
 	
 	//for(i; i<=sizeof(opts_array); i++){
 		hash_data_copy(ret, TYPE_STRINGT, zmq_opt_ident,   config, HK(identity));
 		if(zmq_opt_ident){
-			if(zmq_setsockopt(userdata->zmq_socket, ZMQ_IDENTITY, zmq_opt_ident, strlen(zmq_opt_ident)) != 0)
-				return error("zmq_setsockopt (identity) failed");
+			if(zmq_setsockopt(fdata->zmq_socket, ZMQ_IDENTITY, zmq_opt_ident, strlen(zmq_opt_ident)) != 0){
+				ret = error("zmq_setsockopt (identity) failed");
+				goto error;
+			}
 		}
 	//}
-
-	if(zmq_act_bind){
-		if(zmq_bind(userdata->zmq_socket, zmq_act_bind) != 0)
-			return error("zmq_bind failed");
-	}else if(zmq_act_connect){
-		if(zmq_connect(userdata->zmq_socket, zmq_act_connect) != 0)
-			return error("zmq_connect failed");
-	}
-	return 0;
-} // }}}
-/*
-static ssize_t zmqb_fast_handler(machine_t *machine, fastcall_header *hargs){ // {{{
-	void                  *data;
-	size_t                 data_size;
-	zmq_msg_t              zmq_msg;
-	zmq_userdata          *userdata          = (zmq_userdata *)machine->userdata;
 	
-	switch(hargs->action){
-		// case ACTION_TRANSFER TODO
-		case ACTION_READ:;
-			fastcall_read *rargs = (fastcall_read *)hargs;
+	if(zmq_act_bind){
+		if(zmq_bind(fdata->zmq_socket, zmq_act_bind) != 0){
+			ret = error("zmq_bind failed");
+			goto error;
+		}
+	}else if(zmq_act_connect){
+		if(zmq_connect(fdata->zmq_socket, zmq_act_connect) != 0){
+			ret = error("zmq_connect failed");
+			goto error;
+		}
+	}
+	
+	*pfdata = fdata;
+	return 0;
+
+error:
+	zeromq_t_destroy(fdata);
+	return ret;
+} // }}}
+
+static ssize_t data_zeromq_t_convert_from(data_t *data, fastcall_convert_from *fargs){ // {{{
+	ssize_t                ret;
+	
+	if(fargs->src == NULL)
+		return -EINVAL; 
+		
+	switch(fargs->format){
+		case FORMAT(hash):;
+			hash_t            *config;
 			
-			if( zmq_msg_init(&zmq_msg) != 0)
-				return error("zmq_msg_init failed");
+			data_get(ret, TYPE_HASHT, config, fargs->src);
+			if(ret != 0)
+				return -EINVAL;
 			
-			if( zmq_recv(userdata->zmq_socket, &zmq_msg, 0) != 0)
-				return -errno;
-			
-			data      = zmq_msg_data(&zmq_msg);
-			data_size = zmq_msg_size(&zmq_msg);
-			
-			
-			if(rargs->buffer == NULL || rargs->offset > data_size)
-				return -EINVAL; // invalid range
-			
-			rargs->buffer_size = MIN(rargs->buffer_size, data_size - rargs->offset);
-			
-			if(rargs->buffer_size == 0)
-				return -1; // EOF
-			
-			memcpy(rargs->buffer, data + rargs->offset, rargs->buffer_size);
-			
-			zmq_msg_close(&zmq_msg);
-			return 0;
-			
-		case ACTION_WRITE:;
-			fastcall_write *wargs = (fastcall_write *)hargs;
-			
-			if( zmq_msg_init_size(&zmq_msg, wargs->buffer_size) != 0)
-				return -errno;
-			
-			data      = zmq_msg_data(&zmq_msg);
-			memcpy(data, wargs->buffer, wargs->buffer_size); // TODO zero copy
-			
-			if( zmq_send(userdata->zmq_socket, &zmq_msg, 0) != 0)
-				return -errno;
-			
-			return 0;
+			return zeromq_t_new((zeromq_t **)&data->ptr, config);
 			
 		default:
 			break;
-	}
+	};
 	return -ENOSYS;
 } // }}}
-*/
-/*static ssize_t zmqb_io_t_multipart_handler(data_t *io_data, void *io_userdata, void *args){ // {{{
-	void                  *data;
-	zmq_msg_t              zmq_msg;
-	machine_t             *machine           = (machine_t *)io_userdata;
-	zmq_userdata          *userdata          = (zmq_userdata *)machine->userdata;
-	fastcall_write        *wargs             = (fastcall_write *)args;
-	
-	if( zmq_msg_init_size(&zmq_msg, wargs->buffer_size) != 0)
-		return -errno;
-	
-	data      = zmq_msg_data(&zmq_msg);
-	memcpy(data, wargs->buffer, wargs->buffer_size); // TODO zero copy
-	
-	if( zmq_send(userdata->zmq_socket, &zmq_msg, ZMQ_SNDMORE) != 0)
-		return -errno;
+static ssize_t data_zeromq_t_free(data_t *data, fastcall_free *fargs){ // {{{
+	if(data->ptr != NULL)
+		zeromq_t_destroy(data->ptr);
 	
 	return 0;
-} // }}}*/
-static ssize_t zmqb_io_t_handler(data_t *data, void *userdata, void *args){ // {{{
-	return -ENOSYS;
-	//return zmqb_fast_handler((machine_t *)userdata, args);
 } // }}}
-static void zmq_buffer_free(void *data, void *hint){ // {{{
-	fastcall_free r_free = { { 2, ACTION_FREE } };
-	data_query((data_t *)hint, &r_free);
-	
-	free(hint);
-} // }}}
-static ssize_t zmqb_handler(machine_t *machine, request_t *request){ // {{{
-	ssize_t                ret;
-	uintmax_t              action;
+static ssize_t data_zeromq_t_read(data_t *data, fastcall_read *fargs){ // {{{
+	void                  *msg_data;
+	size_t                 msg_data_size;
 	zmq_msg_t              zmq_msg;
-	data_t                *buffer;
-	data_t                 zmq_iot           = DATA_IOT(machine, &zmqb_io_t_handler);
-	//data_t                 zmq_iot_multi     = DATA_IOT(machine, &zmqb_io_t_multipart_handler);
-	zmq_userdata          *userdata          = (zmq_userdata *)machine->userdata;
+	zeromq_t              *fdata             = (zeromq_t *)data->ptr;
 	
-	hash_data_copy(ret, TYPE_UINTT, action, request, HK(action));
-	if(ret != 0)
+	if(fdata == NULL)
 		return -EINVAL;
 	
-	switch(action){
-		case ACTION_READ:;
-			buffer = hash_data_find(request, HK(buffer));
-			fastcall_transfer r_transfer1 = { { 3, ACTION_TRANSFER }, buffer };
-			return data_query(&zmq_iot, &r_transfer1 );
-			
-		case ACTION_WRITE:;
-			data_t                *flatbuffer;
-			
-			if( (flatbuffer = malloc(sizeof(data_t))) == NULL)
-				return -ENOMEM;
-			
-			flatbuffer->type = TYPE_RAWT;
-			flatbuffer->ptr  = NULL;
-			
-			fastcall_alloc r_alloc = { { 3, ACTION_ALLOC }, DEF_BUFFER_SIZE };
-			if(data_query(flatbuffer, &r_alloc) < 0)
-				return -ENOMEM;
-			
-			buffer = hash_data_find(request, HK(buffer));
-			fastcall_transfer r_transfer2  = { { 3, ACTION_TRANSFER }, flatbuffer };
-			ret = data_query(buffer, &r_transfer2);
-			
-			fastcall_physicallen r_len = { { 3, ACTION_LOGICALLEN } };
-			fastcall_getdataptr  r_ptr = { { 3, ACTION_GETDATAPTR } };
-			if( data_query(flatbuffer, &r_len) != 0 || data_query(flatbuffer, &r_ptr) != 0)
-				return -EFAULT;
-			
-			if( zmq_msg_init_data(&zmq_msg, r_ptr.ptr, r_len.length, &zmq_buffer_free, flatbuffer) != 0)
-				return -errno;
-			
-			if( zmq_send(userdata->zmq_socket, &zmq_msg, 0) != 0)
-				return -errno;
-			
-			return ret;
-			
-		default:
-			break;
-	}
-	return -ENOSYS;
+	if( zmq_msg_init(&zmq_msg) != 0)
+		return error("zmq_msg_init failed");
+	
+	if( zmq_recv(fdata->zmq_socket, &zmq_msg, 0) != 0)
+		return -errno;
+	
+	msg_data      = zmq_msg_data(&zmq_msg);
+	msg_data_size = zmq_msg_size(&zmq_msg);
+	
+	if(fargs->buffer == NULL || fargs->offset > msg_data_size)
+		return -EINVAL; // invalid range
+	
+	fargs->buffer_size = MIN(fargs->buffer_size, msg_data_size - fargs->offset);
+	
+	if(fargs->buffer_size == 0)
+		return -1; // EOF
+	
+	memcpy(fargs->buffer, msg_data + fargs->offset, fargs->buffer_size);
+	
+	zmq_msg_close(&zmq_msg);
+	return 0;
+} // }}}
+static ssize_t data_zeromq_t_write(data_t *data, fastcall_write *fargs){ // {{{
+	void                  *msg_data;
+	zmq_msg_t              zmq_msg;
+	zeromq_t              *fdata             = (zeromq_t *)data->ptr;
+	
+	if( zmq_msg_init_size(&zmq_msg, fargs->buffer_size) != 0)
+		return -errno;
+	
+	msg_data = zmq_msg_data(&zmq_msg);
+	memcpy(msg_data, fargs->buffer, fargs->buffer_size);
+	
+	if( zmq_send(fdata->zmq_socket, &zmq_msg, 0) != 0)
+		return -errno;
+	
+	return 0;
+} // }}}
+static ssize_t data_zeromq_t_transfer(data_t *data, fastcall_transfer *fargs){ // {{{
+	ssize_t                ret;
+	void                  *msg_data;
+	size_t                 msg_data_size;
+	zmq_msg_t              zmq_msg;
+	zeromq_t              *fdata             = (zeromq_t *)data->ptr;
+	
+	if(fdata == NULL)
+		return -EINVAL;
+	
+	if( zmq_msg_init(&zmq_msg) != 0)
+		return error("zmq_msg_init failed");
+	
+	if( zmq_recv(fdata->zmq_socket, &zmq_msg, 0) != 0)
+		return -errno;
+	
+	msg_data      = zmq_msg_data(&zmq_msg);
+	msg_data_size = zmq_msg_size(&zmq_msg);
+	
+	fastcall_write r_write = {
+		{ 5, ACTION_WRITE },
+		0,
+		msg_data,
+		msg_data_size
+	};
+	ret = data_query(fargs->dest, &r_write);
+	
+	zmq_msg_close(&zmq_msg);
+	return ret;
 } // }}}
 
-static machine_t zmq_proto = {                 // NOTE need static or unique name
-	.class          = "modules/c_zmq",
-	.supported_api  = API_HASH,
-	.func_init      = &zmqb_init,
-	.func_configure = &zmqb_configure,
-	.func_destroy   = &zmqb_destroy,
-	.machine_type_hash = {
-		.func_handler = &zmqb_handler
-	},
+data_proto_t zmq_proto = {
+	.type_str               = ZEROMQT_NAME,
+	.api_type               = API_HANDLERS,
+	.handlers               = {
+		[ACTION_CONVERT_FROM]   = (f_data_func)&data_zeromq_t_convert_from,
+		[ACTION_FREE]           = (f_data_func)&data_zeromq_t_free,
+		[ACTION_READ]           = (f_data_func)&data_zeromq_t_read,
+		[ACTION_WRITE]          = (f_data_func)&data_zeromq_t_write,
+		[ACTION_TRANSFER]       = (f_data_func)&data_zeromq_t_transfer,
+	}
 };
 
 int main(void){
-	class_register(&zmq_proto);
+	data_register(&zmq_proto);
 	return 0;
 }
