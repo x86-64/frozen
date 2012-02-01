d072e5b7c2acb9f1715a402d56db72f71027f1ba
x86 <x86mail@gmail.com>
2012-02-01 11:42:54 +0400

    list_t convert_from/to format binary

--- src/libfrozen/data/storage/list/list_t.c
+++ src/libfrozen/data/storage/list/list_t.c
@@ -3,6 +3,9 @@
 
 #include <enum/format/format_t.h>
 #include <core/hash/hash_t.h>
+#include <modifiers/slider/slider_t.h>
+
+#define DATATYPE_BIN uint16_t
 
 static ssize_t data_list_t_alloc(data_t *data, fastcall_alloc *fargs){ // {{{
 	if( (data->ptr = list_t_alloc()) == NULL)
@@ -10,8 +13,51 @@ static ssize_t data_list_t_alloc(data_t *data, fastcall_alloc *fargs){ // {{{
 	
 	return 0;
 } // }}}
+static ssize_t data_list_t_convert_to(data_t *src, fastcall_convert_to *fargs){ // {{{
+	ssize_t                ret               = 0;
+	ssize_t                qret;
+	list_chunk_t          *chunk;
+	list_t                *fdata             = (list_t *)src->ptr;
+	data_t                 d_sl_data         = DATA_SLIDERT(fargs->dest, 0);
+	
+	switch(fargs->format){
+		case FORMAT(binary):;
+			DATATYPE_BIN           type;
+			
+			for(chunk = fdata->head; chunk; chunk = chunk->cnext){
+				type = (DATATYPE_BIN)chunk->data.type;
+				
+				// write type
+				fastcall_write r_write = { { 5, ACTION_WRITE }, 0, &type, sizeof(type) };
+				if( (ret = data_query(&d_sl_data, &r_write)) < 0)
+					break;
+				
+				data_slider_t_freeze(&d_sl_data);
+					
+					// write data
+					fastcall_convert_to r_convert = { { 4, ACTION_CONVERT_TO }, &d_sl_data, FORMAT(binary) };
+					if( (ret = data_query(&chunk->data, &r_convert)) < 0)
+						break;
+				
+				data_slider_t_unfreeze(&d_sl_data);
+			}
+			
+			// terminate list
+			type = __MAX(DATATYPE_BIN);
+			fastcall_write r_write = { { 5, ACTION_WRITE }, 0, &type, sizeof(type) };
+			if( (qret = data_query(&d_sl_data, &r_write)) < 0)
+				ret = qret;
+			break;
+			
+		default:
+			return -ENOSYS;
+	}
+	return ret;
+} // }}}
 static ssize_t data_list_t_convert_from(data_t *dst, fastcall_convert_from *fargs){ // {{{
-	ssize_t                ret;
+	ssize_t                ret               = 0;
+	list_t                *fdata;
+	data_t                 d_sl_data         = DATA_SLIDERT(fargs->src, 0);
 	
 	switch(fargs->format){
 		case FORMAT(clean):
@@ -28,10 +74,45 @@ static ssize_t data_list_t_convert_from(data_t *dst, fastcall_convert_from *farg
 			
 			return data_list_t_alloc(dst, NULL);
 			
-		default:
+		case FORMAT(binary):;
+			DATATYPE_BIN           type;
+			data_t                 item;
+			
+			if((fdata = dst->ptr) == NULL){
+				if( (fdata = dst->ptr = list_t_alloc()) == NULL)
+					return -ENOMEM;
+			}
+			
+			while(1){
+				// read type
+				fastcall_read r_read = { { 5, ACTION_READ }, 0, &type, sizeof(type) };
+				if( (ret = data_query(&d_sl_data, &r_read)) < 0)
+					break;
+				
+				if(type == __MAX(DATATYPE_BIN))
+					break;
+				
+				item.type = type;
+				item.ptr  = NULL;
+				
+				data_slider_t_freeze(&d_sl_data);
+					
+					// read data
+					fastcall_convert_from r_convert = { { 4, ACTION_CONVERT_FROM }, &d_sl_data, FORMAT(binary) };
+					if( (ret = data_query(&item, &r_convert)) < 0)
+						break;
+				
+				data_slider_t_unfreeze(&d_sl_data);
+				
+				if( (ret = list_t_push(fdata, &item)) < 0)
+					break;
+			}
 			break;
+
+		default:
+			return -ENOSYS;
 	};
-	return -ENOSYS;
+	return ret;
 } // }}}
 static ssize_t data_list_t_free(data_t *data, fastcall_free *fargs){ // {{{
 	list_t                *fdata             = (list_t *)data->ptr;
@@ -66,6 +147,7 @@ data_proto_t list_t_proto = {
 	.type_str               = "list_t",
 	.api_type               = API_HANDLERS,
 	.handlers               = {
+		[ACTION_CONVERT_TO]   = (f_data_func)&data_list_t_convert_to,
 		[ACTION_CONVERT_FROM] = (f_data_func)&data_list_t_convert_from,
 		[ACTION_ALLOC]        = (f_data_func)&data_list_t_alloc,
 		[ACTION_FREE]         = (f_data_func)&data_list_t_free,
