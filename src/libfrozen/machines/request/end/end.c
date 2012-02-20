e611f6b277289304f0558772c8ad82f4252179fd
x86 <x86mail@gmail.com>
2012-02-20 20:20:17 +0400

    Fast request/end handler

--- src/libfrozen/machines/request/end/end.c
+++ src/libfrozen/machines/request/end/end.c
@@ -29,34 +29,6 @@ typedef struct end_userdata {
 	data_t                 ret;
 } end_userdata;
 
-static int end_init(machine_t *machine){ // {{{
-	end_userdata         *userdata;
-	
-	if((userdata = machine->userdata = calloc(1, sizeof(end_userdata))) == NULL)
-		return error("calloc failed");
-	
-	return 0;
-} // }}}
-static int end_destroy(machine_t *machine){ // {{{
-	end_userdata      *userdata = (end_userdata *)machine->userdata;
-	
-	data_free(&userdata->ret);
-	free(userdata);
-	return 0;
-} // }}}
-static int end_configure(machine_t *machine, config_t *config){ // {{{
-	ssize_t                ret;
-	data_t                 d_uint            = DATA_UINTT(0);
-	end_userdata          *userdata          = (end_userdata *)machine->userdata;
-	
-	hash_holder_consume(ret, userdata->ret, config, HK(return));
-	if(ret != 0){
-		fastcall_copy r_copy = { { 3, ACTION_COPY }, &userdata->ret };
-		ret = data_query(&d_uint, &r_copy);
-	}
-	return ret;
-} // }}}
-
 static ssize_t end_handler(machine_t *machine, request_t *request){ // {{{
 	ssize_t                ret               = 0;
 	ssize_t                ret2;
@@ -70,6 +42,36 @@ static ssize_t end_handler(machine_t *machine, request_t *request){ // {{{
 	
 	return ret;
 } // }}}
+static ssize_t end_handler_default(machine_t *machine, request_t *request){ // {{{
+	return 0;
+} // }}}
+
+static int end_init(machine_t *machine){ // {{{
+	end_userdata         *userdata;
+	
+	if((userdata = machine->userdata = calloc(1, sizeof(end_userdata))) == NULL)
+		return error("calloc failed");
+	
+	data_set_void(&userdata->ret);
+	return 0;
+} // }}}
+static int end_destroy(machine_t *machine){ // {{{
+	end_userdata      *userdata = (end_userdata *)machine->userdata;
+	
+	data_free(&userdata->ret);
+	free(userdata);
+	return 0;
+} // }}}
+static int end_configure(machine_t *machine, config_t *config){ // {{{
+	ssize_t                ret;
+	end_userdata          *userdata          = (end_userdata *)machine->userdata;
+	
+	hash_holder_consume(ret, userdata->ret, config, HK(return));
+	if(ret == 0)
+		machine->machine_type_hash.func_handler = &end_handler;
+	
+	return 0;
+} // }}}
 
 machine_t end_proto = {
 	.class          = "request/end",
@@ -78,7 +80,7 @@ machine_t end_proto = {
 	.func_destroy   = &end_destroy,
 	.func_configure = &end_configure,
 	.machine_type_hash = {
-		.func_handler = &end_handler
+		.func_handler = &end_handler_default
 	},
 };
 
