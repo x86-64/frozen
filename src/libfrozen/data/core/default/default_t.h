4238aa36bd5a88345f477fafdd8be8144c616130
x86 <x86mail@gmail.com>
2012-07-02 22:23:28 +0000

    Add data_default_lookup in headers

--- src/libfrozen/data/core/default/default_t.h
+++ src/libfrozen/data/core/default/default_t.h
@@ -22,6 +22,7 @@ ssize_t       data_default_convert_from  (data_t *dest, fastcall_convert_from *f
 ssize_t       data_default_free          (data_t *data, fastcall_free *fargs);
 ssize_t       data_default_control       (data_t *data, fastcall_control *fargs);
 ssize_t       data_default_consume       (data_t *data, fastcall_consume *fargs);
+ssize_t       data_default_lookup        (data_t *data, fastcall_lookup *fargs);
 ssize_t       data_default_enum          (data_t *data, fastcall_enum *fargs);
 
 #endif
